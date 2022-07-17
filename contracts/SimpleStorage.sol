f66266fa565494754455f4f25cfadf283248b32e
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-07-17 13:13:45 +0200

    Reduce code

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,22 +1,22 @@
 contract SimpleStorage {
 
-    uint256 public favoriteNumber;
+    uint256 public favoriteNum;
 
     struct Person {
         string name;
-        uint256 favoriteNumber;
+        uint256 favoriteNum;
     }
 
-    Person[] public getPersonAndNumber;
+    Person[] public getPersonAndNum;
 
-    mapping (string => uint256) public getPersonNumber;
+    mapping (string => uint256) public getPersonNum;
 
-    function setPerson(string memory _name, uint256 _favoriteNumber) external {
-        getPersonAndNumber.push(Person(_name, _favoriteNumber));
-        getPersonNumber[_name] = _favoriteNumber;
+    function setPerson(string memory _name, uint256 _favoriteNum) external {
+        getPersonAndNum.push(Person(_name, _favoriteNum));
+        getPersonNum[_name] = _favoriteNum;
     }
 
-    function setFavoriteNumber(uint256 _favoriteNumber) external {
-        favoriteNumber = _favoriteNumber;
+    function setFavoriteNumber(uint256 _favoriteNum) external {
+        favoriteNum = _favoriteNum;
     }
 }
